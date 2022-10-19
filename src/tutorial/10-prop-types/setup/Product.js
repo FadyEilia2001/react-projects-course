fcc65ffda3c7e977d072c704626d44e28ca9dd3e
FadyEilia2001 <feilia@WGU.edu>
2022-10-19 11:02:16 -0500

    working on useCallback

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+import defaultImage from "../../../assets/default-image.jpeg";
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = ({ image, name, price }) => {
+  const url = image && image.url;
+  return (
+    <article className="product">
+      <img src={url || image} alt={name} />
+      <h4>{name}</h4>
+      <p>${price}</p>
+    </article>
+  );
+};
+
+Product.propTypes = {
+  image:PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+};
+
+Product.defaultProps = {
+  name: "defaultname",
+  price: 3.88,
+  image: defaultImage,
 };
 
 export default Product;
---
+++ src/tutorial/6-useReducer/setup/index.jsx
@@ -0,0 +1,72 @@
+import React, { useState, useReducer } from "react";
+import Modal from "./Modal";
+import { data } from "../../../data";
+// reducer function
+import { reducer } from "./reducer";
+
+const initalState = {
+  people: [],
+  isModalOpen: false,
+  modalContent: "",
+};
+
+const Index = () => {
+  const [name, setName] = useState("");
+  const [state, dispatch] = useReducer(reducer, initalState);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name) {
+      const newItem = { id: new Date().getTime().toString(), name };
+      dispatch({
+        type: "ADD_ITEM",
+        payload: newItem,
+      });
+      setName("");
+    } else {
+      dispatch({ type: "NO_VALUE" });
+    }
+  };
+
+  const closeModal = () => {
+    dispatch({ type: "CLOSE_MODAL" });
+  };
+  return (
+    <>
+      {state.isModalOpen && (
+        <Modal closeModal={closeModal} modalContent={state.modalContent} />
+      )}
+      <form onSubmit={handleSubmit} className="form">
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button
+          type="submit"
+          style={{
+            display: "block",
+            margin: "20px auto",
+            padding: "5px 30px",
+          }}
+        >
+          Add
+        </button>
+      </form>
+      {state.people.map((person) => (
+        <div key={person.id} className="item">
+          <h4>{person.name}</h4>
+          <button
+            onClick={() =>
+              dispatch({ type: "REMOVE_ITEM", payload: person.id })
+            }
+          >
+            Remove
+          </button>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Index;
