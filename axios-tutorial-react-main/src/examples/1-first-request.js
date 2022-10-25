6fbc74ad09d8c098c40010422fb2dd0579ca651b
FadyEilia2001 <feilia@WGU.edu>
2022-10-25 17:05:50 -0500

    working on axios

--- axios-tutorial-react-main/src/examples/1-first-request.js
+++ axios-tutorial-react-main/src/examples/1-first-request.js
@@ -1,12 +1,23 @@
-import { useEffect } from 'react';
+import axios from "axios";
+import { useEffect } from "react";
 // limit, if 429 wait for 15 min and try again
-const url = 'https://course-api.com/react-store-products';
+const url = "https://course-api.com/react-store-product";
 
 const FirstRequest = () => {
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(url);
+
+      console.log(response);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
   useEffect(() => {
-    console.log('first axios request');
+    fetchData();
   }, []);
 
-  return <h2 className='text-center'>first request</h2>;
+  return <h2 className="text-center">first request</h2>;
 };
 export default FirstRequest;
